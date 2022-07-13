c4a495badf396bc6935c0835a36e7b8a9525f177
fatonianggris <mfatonianggris@gmail.com>
2022-07-13 16:59:29 +0700

    rev4  only view

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,6 @@ import './assets/css/main/mobile2.css';
 import './assets/css/main/mobile.css';
 
 import './assets/js/jquery-3.4.1.min.js'
-import './assets/js/bootstrap.bundle.min.js'
-import './assets/js/owl.carousel.min.js'
-import './assets/js/lity.min.js'
-import './assets/js/bootstrap-select.min.js'
-import './assets/js/jquery.marquee.min.js'
 
 const app = createApp(Main);
 app.use(Router);
